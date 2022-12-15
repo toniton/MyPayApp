94e712291fc3390c9bd6de67d3e3dc4b98046f70
toniton <akinjiolatoni@gmail.com>
2022-12-15 19:26:34 +0100

    hide navigation on login screen

--- App.tsx
+++ App.tsx
@@ -64,7 +64,11 @@ const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
       {/* <SafeAreaView style={backgroundStyle}>
