41c2483cd25d82fd90f38e8a3fc7aee78839890f
toniton <akinjiolatoni@gmail.com>
2022-12-15 19:21:53 +0100

    styled login screen

--- screens/login/index.tsx
+++ screens/login/index.tsx
@@ -1,44 +1,50 @@
-import React, { useState, useCallback } from "react";
-import {
-  SafeAreaView,
-  TextInput,
-  Text,
-  Button,
-  View,
-} from 'react-native';
+import React, {useState, useCallback} from 'react';
+import {SafeAreaView, TextInput, Text, Button, View, Alert} from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {styles, VARIANTS} from '../../styles';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
-import { useAppStore } from "../../App";
+export const LoginScreen = ({navigation}) => {
+  const [name, setName] = useState<string>('Hi');
 
-export const LoginScreen = ({ navigation }) => {
-    const [name, setName] = useState("");
+  const onChangeText = (inputText: string) => {
+    console.log(inputText);
+    setName(() => inputText);
+  };
 
-    const onChangeText = (inputText) => {
-        setName(()=> inputText);
+  const onSubmit = useCallback(async () => {
+    try {
+      await AsyncStorage.setItem('@user', name);
+      navigation.navigate('Home');
+    } catch (e) {
+      Alert.alert('Login failed', 'an error occurred trying to login');
     }
+    // TODO: Set the name in global storage
+    // TODO: Navigate to the home screen
+  }, [name, navigation]);
 
-    const setAppName = useAppStore((state) => state.setName);
-    const onSubmit = useCallback(() => {
-        setAppName(name);
-        navigation.navigate('Home');
-        // TODO: Set the name in global storage
-        // TODO: Navigate to the home screen
-    }, [name])
-
-    return (<SafeAreaView style={{flex: 1}}>
-        <View style={{ flex: 1, alignItems: 'stretch', justifyContent: 'center' }}>
-            <Text>Login Screen</Text>
-
-            <Text>Name</Text>
-            <TextInput
-                style={{ backgroundColor: "red", height: 60 }}
-                onChangeText={onChangeText}
-                // value={name}
-            />
-            <Button
-                onPress={onSubmit}
-                title="Login"
-            />
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.section}>
+          <Text style={styles.label}>Username</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={onChangeText}
+            textContentType="username"
+            value={name}
+            placeholder="Username"
+          />
         </View>
-      </SafeAreaView>
-    );
-  }
+        <View style={styles.button}>
+          <Button
+            color={Colors[VARIANTS.PRIMARY_TX]}
+            onPress={onSubmit}
+            title="Login"
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
--- styles.ts
+++ styles.ts
@@ -1,6 +1,6 @@
 import {StyleSheet} from 'react-native';
 
-enum SIZES {
+export enum SIZES {
   XS,
   S,
   M,
@@ -9,13 +9,34 @@ enum SIZES {
   XXL,
 }
 
-const Dimensions = {
+export const Dimensions = {
   [SIZES.XS]: 2,
   [SIZES.S]: 4,
   [SIZES.M]: 8,
   [SIZES.L]: 16,
   [SIZES.XL]: 32,
   [SIZES.XXL]: 64,
+};
+
+export enum VARIANTS {
+  WHITE,
+  BLACK,
+  SURFACE_BG,
+  SURFACE_TX,
+  PRIMARY_BG,
+  PRIMARY_TX,
+  SECONDARY_BG,
+  SECONDARY_TX,
+}
+
+const Colors = {
+  [VARIANTS.WHITE]: '#FFFFFF',
+  [VARIANTS.BLACK]: '#000000',
+  [VARIANTS.SURFACE_BG]: '#F4F4F4',
+  [VARIANTS.SURFACE_TX]: '#353535',
+  [VARIANTS.PRIMARY_BG]: '#000000',
+  [VARIANTS.PRIMARY_TX]: '#FAFAFA',
+  [VARIANTS.SECONDARY_TX]: '#70757a',
 };
 
 export const styles = StyleSheet.create({
@@ -35,10 +56,46 @@ export const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+  container: {
+    paddingHorizontal: Dimensions[SIZES.XL],
+    flex: 1,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    backgroundColor: Colors[VARIANTS.SURFACE_BG],
+  },
+  section: {
+    marginTop: Dimensions[SIZES.L],
+    // paddingHorizontal: Dimensions[SIZES.XL],
+  },
+  title: {
+    color: Colors[VARIANTS.SURFACE_TX],
+    fontSize: Dimensions[SIZES.XL],
+    marginBottom: Dimensions[SIZES.L],
+  },
+  label: {
+    color: Colors[VARIANTS.SECONDARY_TX],
+    fontSize: Dimensions[SIZES.L],
+    marginBottom: Dimensions[SIZES.M],
+  },
   input: {
-    height: Dimensions[SIZES.XL],
-    margin: Dimensions[SIZES.L],
+    height: Dimensions[SIZES.XXL],
     borderWidth: Dimensions[SIZES.XS],
+    borderColor: Colors[VARIANTS.SECONDARY_TX],
     padding: Dimensions[SIZES.L],
+    backgroundColor: Colors[VARIANTS.WHITE],
+    borderRadius: Dimensions[SIZES.L],
+    color: Colors[VARIANTS.SURFACE_TX],
+    fontSize: Dimensions[SIZES.L],
+  },
+  button: {
+    height: Dimensions[SIZES.XXL],
+    borderWidth: Dimensions[SIZES.XS],
+    justifyContent: 'center',
+    marginTop: Dimensions[SIZES.L],
+    backgroundColor: Colors[VARIANTS.PRIMARY_BG],
+    borderColor: Colors[VARIANTS.SECONDARY_TX],
+    borderRadius: Dimensions[SIZES.L],
+    color: Colors[VARIANTS.PRIMARY_TX],
+    fontSize: Dimensions[SIZES.L],
   },
 });
